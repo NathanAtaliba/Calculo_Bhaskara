d51d4fdd6051017193cee0598b91ca49d2a908d0
NathanAtaliba <100451579+NathanAtaliba@users.noreply.github.com>
2022-09-29 00:27:16 -0300

    Calculo_Bhaskara

---
+++ script.js
@@ -0,0 +1,7 @@
+function calcular(){
+var X1 = window.document.getElementById("X1")
+var X2 = window.document.getElementById("X2")
+var DELTA = window.document.getElementById("DELTA")
+X1.innerHTML=""
+
+}
